ea180e6cb18e1e02bc0708518f04648218bbfe8d
Tajim Islam <tjm4181@gmail.com>
2024-03-20 14:05:32 +0600

    'datatable_responsive'

--- resources/views/backend/payment_method/trash_list.blade.php
+++ resources/views/backend/payment_method/trash_list.blade.php
@@ -33,20 +33,22 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <table id="datatables-reponsive" class="table table-striped" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>@lang('common.sl')</th>
-                                    <th>@lang('payment_method.method_name')</th>
-                                    <th>@lang('payment_method.number')</th>
-                                    <th>@lang('common.status')</th>
-                                    <th>@lang('common.actions')</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        <div class="table-responsive">
+                            <table id="datatables-reponsive" class="table table-striped" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>@lang('common.sl')</th>
+                                        <th>@lang('payment_method.method_name')</th>
+                                        <th>@lang('payment_method.number')</th>
+                                        <th>@lang('common.status')</th>
+                                        <th>@lang('common.actions')</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
