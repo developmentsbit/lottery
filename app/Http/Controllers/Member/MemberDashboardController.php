9b89428500ccae4b572aa5b9ec06f377882d4731
tanim0299 <tanimchy417@gmail.com>
2024-07-08 15:44:59 +0600

    new_work_8-7-24

--- app/Http/Controllers/Member/MemberDashboardController.php
+++ app/Http/Controllers/Member/MemberDashboardController.php
@@ -306,9 +306,8 @@ class MemberDashboardController extends Controller
     public function lottery_history()
     {
         $param['data'] = GameLedger::withTrashed()
-        ->leftjoin('game_setups','game_setups.id','game_ledgers.game_id')
+        ->join('game_setups','game_setups.id','game_ledgers.game_id')
         ->where('game_ledgers.member_id',Auth::guard('member')->user()->member_id)
-        ->where('game_setups.status',1)
         ->orderBy('date','DESC')
         ->select('game_ledgers.*')
         ->get();
